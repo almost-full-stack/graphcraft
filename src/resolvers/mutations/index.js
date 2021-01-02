f798680e387829e2ce4fe275eb81576f51d606b3
Ali Rizwan <ali.aruz@outlook.com>
2021-01-02 05:26:57 +0100

    simplified mutations

---
+++ src/resolvers/mutations/index.js
@@ -0,0 +1,4 @@
+module.exports.create = require('./create');
+module.exports.update = require('./update');
+module.exports.destroy = require('./destroy');
+module.exports.restore = require('./restore');
