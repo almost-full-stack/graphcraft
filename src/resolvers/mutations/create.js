ae67766fa3c80124dfbf3ede9bcc8362e3e7d508
Ali Rizwan <ali.aruz@outlook.com>
2021-07-11 19:43:13 +0200

    fixed issue with uuid module

--- src/resolvers/mutations/create.js
+++ src/resolvers/mutations/create.js
@@ -1,4 +1,4 @@
-const uuid = require('uuid/v4');
+const { v4: uuid } = require('uuid');
 const { keysWhichAreModelAssociations } = require('../../utils');
 
 async function createMutation (graphqlParams, mutationOptions) {
